8a5627bf0fde6b611b5bd8b78d2cf8e318711474
Svetlana <sveta.soroka1997@gmail.com>
2020-03-07 14:58:32 +0300

    Lemmatization

--- first_task_search_engine/prepare_search_index.py
+++ first_task_search_engine/prepare_search_index.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from text_preparation import tokenize
+from text_preparation import tokenize, lemmatize
+
 
 df = pd.read_csv("./data/prepared_dataset.csv")
-df = df.iloc[:10]
+df = df.iloc[:3]
 df = tokenize(df)
+df = lemmatize(df)
--- first_task_search_engine/text_preparation.py
+++ first_task_search_engine/text_preparation.py
@@ -1,9 +1,18 @@
 import pandas as pd
+from nltk.stem import WordNetLemmatizer
 
 
 def tokenize(df: pd.DataFrame(columns=["Text"])):
     df["Text"].replace(r"(<[^>]*>|\W)", " ", regex=True, inplace=True)
     df["Text"].replace(r" +", " ", regex=True, inplace=True)
     df["Text"] = df["Text"].astype(str).str.lower()
-    df["Text"] = df["Text"].apply(lambda x: x.split(" "))
+    df["Text"] = df["Text"].apply(lambda text: text.split(" "))
+    return df
+
+
+def lemmatize(df: pd.DataFrame(columns=["Text"])):
+    lemmatizer = WordNetLemmatizer()
+    df["Text"] = df["Text"].apply(
+        lambda tokens_list: [lemmatizer.lemmatize(w) for w in tokens_list]
+    )
     return df
